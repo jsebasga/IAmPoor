b84b8cd5f4cd9b939ded4b883985919fe7c6ad22
Sebastian Güiza <juanitoguiza@gmail.com>
2022-07-06 09:02:50 -0500

    Load I am Poor my second app in IOS

---
+++ README.md
@@ -0,0 +1,10 @@
+
+I Am Poor, Developed by Sebastian Güiza, using Xcode and App Brewery
+
+#  *** I Am Rich ***
+
+This is I Am Poor and is the second app that I made to use with iOS
+
+# Enjoy it !!!
+
+![Main Screen](IAmPoorScreen.png)
---
+++ iAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  iAmPoor
+//
+//  Created by Sebastian Güiza 11-04-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
